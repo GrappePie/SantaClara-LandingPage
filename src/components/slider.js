15c6107e81a24bbaf76bc919ed14a8d9b57c8de9
GrappePie <53881734+GrappePie@users.noreply.github.com>
2023-06-24 13:32:48 -0600

    deploy changes

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,13 +1,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import { Image } from "next/image"
+import Image from 'next/image';
 
 const slider = ({ images }) => {
     return (
         <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true} dynamicHeight={true} >
             {images.map((image, index) => (
                 <div key={index}>
-                    <Image src={image.url} alt=""/>
+                    <Image src={image.url} alt="" width={image.width} height={image.height*0.70}/>
                 </div>
             ))}
         </Carousel>
